c469017ac407f3a340397bff0f3b58b815f84dd7
Arda Burak Ekmekçi <ardaburak1992@hotmail.com>
2019-08-26 00:37:48 +0300

    Character info
    Character info displayed on the secreen

--- wowsome/src/AppNavbar.js
+++ wowsome/src/AppNavbar.js
@@ -8,7 +8,16 @@ class AppNavbar extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand">Navbar</a>
+          <a className="navbar-brand mx-5">
+            <img
+              src="https://listimg.pinclipart.com/picdir/s/30-305286_bird-icon-noto-animals-nature-iconset-google-google.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            ></img>
+            Wowsome
+          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,6 +31,9 @@ class AppNavbar extends Component {
           </button>
           <NavbarSearchComponents params={this.props} />
         </nav>
+        <div>
+          <DisplayCharacterData params={this.props} />
+        </div>
       </div>
     );
   }
@@ -31,46 +43,124 @@ class NavbarSearchComponents extends Component {
   render() {
     return (
       <div className="collapse navbar-collapse" id="navbarSearchToggler">
-        <label htmlFor="region">Region:</label>
-        <select
-          className="form-control mx-2"
-          id="region"
-          onChange={event => {
-            this.props.params.setRegionData(event.target.value);
-          }}
-        >
-          <option>{this.props.params.Navbar.regionList.option1}</option>
-          <option>{this.props.params.Navbar.regionList.option2}</option>
-        </select>
-        <label htmlFor="realm">Realm:</label>
-        <select
-          className="form-control mx-2"
-          id="realm"
-          onChange={event => {
-            this.props.params.setRealmData(event.target.value);
-          }}
-        >
-          <option>{this.props.params.Navbar.realmList.option1}</option>
-          <option>{this.props.params.Navbar.realmList.option2}</option>
-          <option>{this.props.params.Navbar.realmList.option3}</option>
-        </select>
-        <input
-          className="form-control mr-sm-2"
-          type="text"
-          placeholder="Character Name"
-          aria-label="Character Name"
-          onChange={event => {
-            this.props.params.setCharacterNameData(event.target.value);
-          }}
-        />
-        <button
-          className="btn btn-outline-success my-2 my-sm-0"
-          onClick={() => {
-            this.props.params.getCharacterData(this.props.params);
-          }}
-        >
-          Search
-        </button>
+        <div className="row">
+          <div className="col-12">
+            <div className="form-inline">
+              <label htmlFor="region" className="control-label">
+                Region:
+              </label>
+              <select
+                className="form-control mx-2 my-2"
+                id="region"
+                onChange={event => {
+                  this.props.params.setRegionData(event.target.value);
+                }}
+              >
+                <option>{this.props.params.Navbar.regionList.option1}</option>
+                <option>{this.props.params.Navbar.regionList.option2}</option>
+              </select>
+              <label htmlFor="realm">Realm:</label>
+              <select
+                className="form-control mx-2 my-2"
+                id="realm"
+                onChange={event => {
+                  this.props.params.setRealmData(event.target.value);
+                }}
+              >
+                <option>{this.props.params.Navbar.realmList.option1}</option>
+                <option>{this.props.params.Navbar.realmList.option2}</option>
+                <option>{this.props.params.Navbar.realmList.option3}</option>
+              </select>
+              <input
+                className="form-control mx-2 my-2"
+                type="text"
+                placeholder="Character Name"
+                aria-label="Character Name"
+                onChange={event => {
+                  this.props.params.setCharacterNameData(event.target.value);
+                }}
+              />
+              <button
+                className="btn btn-outline-success mx-2 my-2"
+                onClick={() => {
+                  this.props.params.getCharacterData(this.props.params);
+                }}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class DisplayCharacterData extends Component {
+  render() {
+    const ConvertToDate = params => {
+      var date = new Date(params.params);
+      return date.toLocaleString();
+    };
+    const ExtractData = () => {
+      return (
+        <div className="container">
+          <h5>
+            Character Name:&ensp;
+            {this.props.params.Navbar.characterData.name}
+            &emsp; Gender:&ensp;
+            {this.props.params.Navbar.characterData.gender.name}
+            <br />
+            <br />
+            Race:&ensp;
+            {this.props.params.Navbar.characterData.race.name}
+            &emsp; Faction:&ensp;
+            {this.props.params.Navbar.characterData.faction.name}
+            <br />
+            <br />
+            Guild:&ensp;
+            {this.props.params.Navbar.characterData.guild.name}
+            <br />
+            <br />
+            <br />
+          </h5>
+          <h6>
+            Class:&nbsp;
+            {this.props.params.Navbar.characterData.character_class.name}
+            &emsp; Spec:&nbsp;
+            {this.props.params.Navbar.characterData.active_spec.name}
+            <br />
+            <br />
+            Avarage Item Level:&nbsp;
+            {this.props.params.Navbar.characterData.average_item_level}
+            &emsp; Equipped Item Level:&nbsp;
+            {this.props.params.Navbar.characterData.equipped_item_level}
+            <br />
+            <br />
+            Achievement Points:&nbsp;
+            {this.props.params.Navbar.characterData.achievement_points}
+            &emsp; Last Login: :&nbsp;
+            <ConvertToDate
+              params={
+                this.props.params.Navbar.characterData.last_login_timestamp
+              }
+            />
+          </h6>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        {!this.props.params.Navbar.isLoaded ? (
+          <div>
+            <p>Loading</p>
+          </div>
+        ) : (
+          <div>
+            <ExtractData />
+          </div>
+        )}
       </div>
     );
   }
--- wowsome/src/store/AppNavbar.js
+++ wowsome/src/store/AppNavbar.js
@@ -1,6 +1,7 @@
 const regionSelectionChangedType = "REGION_CHANGED";
 const regionSelectionRealmChangedType = "REALM_CHANGED";
 const characterNameChangedType = "CHARACTER_NAME_CHANGED";
+const characterDataReceivedType = "CHARACTER_DATA_RECEIVED";
 
 const initialState = {
   type: "navbar-search",
@@ -9,9 +10,15 @@ const initialState = {
   realm: "kilrogg",
   characterName: "alchemy",
   regionList: { option1: "eu", option2: "us" },
-  realmList: { option1: "kilrogg", option2: "runetotem", option3: "nagrand" }
+  realmList: { option1: "kilrogg", option2: "runetotem", option3: "nagrand" },
+  isLoaded: false
 };
-
+/*
+  Using https://cors-anywhere.herokuapp.com/ to get around CORS.
+  Building up the url
+  Make a request and get the data. ToDo: Save data.
+  set methods are used to save data to store whenever user changes any input on the navbar.
+*/
 export const actionCreators = {
   getCharacterData: input => async dispatch => {
     try {
@@ -23,23 +30,48 @@ export const actionCreators = {
         "?namespace=profile-eu&locale=en_US&access_token=" +
         input.OAuth.access_token;
       console.log(url);
-      fetch(url).then(results => {
-        console.log(results.json());
-      });
+      fetch(url)
+        .then(function(results) {
+          return results.json();
+        })
+        .then(function(json) {
+          if (json.code === 404) {
+            dispatch({
+              type: characterDataReceivedType,
+              payload: {
+                response: "This character does not exists!",
+                isLoaded: false
+              }
+            });
+          } else {
+            dispatch({
+              type: characterDataReceivedType,
+              payload: { response: json, isLoaded: true }
+            });
+          }
+        });
     } catch (E) {
-      console.log(E.response);
+      console.log("ERROR: ", E.response);
+      dispatch({
+        type: characterDataReceivedType,
+        payload: { response: "Something went wrong!", isLoaded: true }
+      });
     }
   },
   setRegionData: input => ({
     type: regionSelectionChangedType,
-    payload: input
+    payload: input.toLowerCase()
   }),
   setRealmData: input => ({
     type: regionSelectionRealmChangedType,
-    payload: input
+    payload: input.toLowerCase()
   }),
   setCharacterNameData: input => ({
     type: characterNameChangedType,
+    payload: input.toLowerCase()
+  }),
+  setCharacterData: input => ({
+    type: characterDataReceivedType,
     payload: input
   })
 };
@@ -52,6 +84,11 @@ export const reducer = (state = initialState, action) => {
       return Object.assign({}, state, { realm: action.payload });
     case characterNameChangedType:
       return Object.assign({}, state, { characterName: action.payload });
+    case characterDataReceivedType:
+      return Object.assign({}, state, {
+        characterData: action.payload.response,
+        isLoaded: action.payload.isLoaded
+      });
     default:
       return state;
   }
